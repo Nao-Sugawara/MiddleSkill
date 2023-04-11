3a3be2db2e32e7e3ca25aea6a1199dad24f89d32
Nao-Sugawara <make.a.wish5115@icloud.com>
2023-04-11 23:05:21 +0900

    課題1-13提出

---
+++ 1-13/src/test1.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class test1 {
+	public static void main(String[] args) {
+		//改行
+		System.out.println("問１");
+		//問題1:下記の条件を使ってfor文と条件文を組み合わせ結果が同じになるよう出力しなさい。for文ネスト3回（3回forを書くこと）
+		List<String> strArray1 = new ArrayList<>(
+				Arrays.asList("f", "u", "k", "u", "d", "a", "k", "a", "n", "e", "k", "o"));
+		List<String> strArray2 = new ArrayList<>();
+		List<String> strArray3 = new ArrayList<>();
+		List<String> strArray4 = new ArrayList<>();
+
+		// strArray1でfor文を回し3文字目(k)以降のものをstrArray2に格納する(1個目のfor文)
+		for (String s : strArray1) {
+			if (s.equals("k")) {
+				strArray2.add(s);
+				break;
+			}
+		}
+		for (int i = strArray1.indexOf("k") + 1; i < strArray1.size(); i++) {
+			strArray2.add(strArray1.get(i));
+		}
+
+		// strArray2でfor文を回しkとaとoの物だけでstrArray3に格納する(2個目のfor文)
+		for (String s : strArray2) {
+			if (s.equals("k") || s.equals("a") || s.equals("o")) {
+				strArray3.add(s);
+			}
+		}
+
+		// strArray3でfor文を回し「kakao」となるようにstrArray4に格納しstrArray4を出力する。(3個目のfor文)
+		for (int i = 0; i < strArray3.size(); i++) {
+			if (strArray3.get(i).equals("k") && i + 3 < strArray3.size() && strArray3.get(i + 1).equals("a")
+					&& strArray3.get(i + 2).equals("k") && strArray3.get(i + 3).equals("o")) {
+				strArray4.addAll(Arrays.asList("k", "a", "k", "a", "o"));
+			}
+		}
+
+		System.out.println(strArray4); // 出力結果: [k, a, k, a, o]
+
+	}
+}
